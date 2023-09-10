359cb3de983d1858dae15b4bf49fb8fce34555ee
Marcos Costa <marcos.lcosta@live.com>
2023-09-10 19:46:13 -0400

    feat: set all expression types

--- src/RinhaCompiler/Interpreter/CompiledFile.cs
+++ src/RinhaCompiler/Interpreter/CompiledFile.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.Json.Serialization;
-using static System.Net.Mime.MediaTypeNames;
+﻿using System.Net;
+using System.Text.Json.Serialization;
 
 namespace RinhaCompiler.Interpreter;
 
@@ -14,6 +14,8 @@ public class Location
 
     [JsonPropertyName("filename")]
     public string FileName { get; set; }
+
+    public string GetLog() => $"Processing file {FileName}, start: {Start}, end {End}";
 }
 
 public abstract class FileLocator
@@ -31,21 +33,24 @@ public class CompiledFile : FileLocator, ICommandExecute
 
     public object Run()
     {
-       return Expression.Run();
+        return Expression.Run();
     }
 }
 
 [JsonPolymorphic(TypeDiscriminatorPropertyName = "kind", IgnoreUnrecognizedTypeDiscriminators = true)]
-[JsonDerivedType(typeof(PrintExpression), typeDiscriminator: nameof(Term.Print))]
-[JsonDerivedType(typeof(CallExpression), typeDiscriminator: nameof(Term.Call))]
-[JsonDerivedType(typeof(FunctionExpression), typeDiscriminator: nameof(Term.Function))]
-[JsonDerivedType(typeof(ConditionExpression), typeDiscriminator: nameof(Term.If))]
 [JsonDerivedType(typeof(VarExpression), typeDiscriminator: nameof(Term.Var))]
-[JsonDerivedType(typeof(BinaryExpression), typeDiscriminator: nameof(Term.Binary))]
+[JsonDerivedType(typeof(FunctionExpression), typeDiscriminator: nameof(Term.Function))]
+[JsonDerivedType(typeof(CallExpression), typeDiscriminator: nameof(Term.Call))]
 [JsonDerivedType(typeof(LetExpression), typeDiscriminator: nameof(Term.Let))]
 [JsonDerivedType(typeof(ValueExpression<string>), typeDiscriminator: nameof(Term.Str))]
 [JsonDerivedType(typeof(ValueExpression<int>), typeDiscriminator: nameof(Term.Int))]
 [JsonDerivedType(typeof(ValueExpression<bool>), typeDiscriminator: nameof(Term.Bool))]
+[JsonDerivedType(typeof(BinaryExpression), typeDiscriminator: nameof(Term.Binary))]
+[JsonDerivedType(typeof(ConditionExpression), typeDiscriminator: nameof(Term.If))]
+[JsonDerivedType(typeof(TupleExpression), typeDiscriminator: nameof(Term.Tuple))]
+[JsonDerivedType(typeof(FirstExpression), typeDiscriminator: nameof(Term.First))]
+[JsonDerivedType(typeof(SecondExpression), typeDiscriminator: nameof(Term.Second))]
+[JsonDerivedType(typeof(PrintExpression), typeDiscriminator: nameof(Term.Print))]
 public abstract class Expression : FileLocator, ICommandExecute
 {
     public abstract object Run();
@@ -59,20 +64,24 @@ public sealed class VarExpression : Expression
     {
         return Text;
     }
+
 }
 
 public sealed class LetExpression : Expression
 {
     public VarExpression Name { get; set; }
     public Expression Next { get; set; }
-
+    public Expression Value { get; set; }
     public override object Run()
     {
-        throw new NotImplementedException();
+        Value?.Run();
+
+        return Next.Run();
     }
 }
 
-public class ValueExpression<T> : Expression
+public abstract class ValueExpression : Expression { }
+public class ValueExpression<T> : ValueExpression
 {
     public T Value { get; set; }
 
@@ -86,17 +95,29 @@ public sealed class PrintExpression : ValueExpression<Expression>
 {
     public override object Run()
     {
-        if(Value.Run() is bool boolValue)
+        var runResult = Value.Run();
+        if (runResult is bool boolValue)
         {
             Console.WriteLine(boolValue);
         }
-        else if (Value.Run() is string strValue)
+        else if (runResult is string strValue)
         {
             Console.WriteLine(strValue);
         }
-        else if(Value.Run() is int intValue)
+        else if (runResult is int intValue)
         {
             Console.WriteLine(intValue);
+        }
+        else if (runResult is Expression expressionValue)
+        {
+            if (expressionValue is PrintExpression) throw new ArgumentException($"Error on processing {Location.GetLog()}");
+            if (expressionValue is TupleExpression tupleExpression)
+            {
+                Console.WriteLine($"({tupleExpression.First.Run()}, {tupleExpression.Second.Run()})");
+                return null!;
+            }
+
+            Console.WriteLine(expressionValue.Run());
         }
         return null!;
     }
@@ -104,29 +125,307 @@ public sealed class PrintExpression : ValueExpression<Expression>
 
 public sealed class FunctionExpression : ValueExpression<Expression>
 {
-    public VarExpression[] Parameters { get; set; }
+    public IEnumerable<Expression> Parameters { get; set; }
+
+    public override object Run()
+    {
+        return Value.Run();
+    }
 }
+
+public sealed class TupleExpression : Expression
+{
+    public Expression First { get; set; }
+    public Expression Second { get; set; }
+    public override object Run()
+    {
+        return (First.Run(), Second.Run());
+    }
+}
+
+public sealed class FirstExpression : ValueExpression<Expression>
+{
+    public override object Run()
+    {
+        if (Value is TupleExpression tupleExpression)
+        {
+            return tupleExpression.First.Run();
+        }
+
+        throw new ArgumentException($"Error on processing {Location.GetLog()}");
+    }
+}
+
+public sealed class SecondExpression : ValueExpression<Expression>
+{
+    public override object Run()
+    {
+        if (Value is TupleExpression tupleExpression)
+        {
+            return tupleExpression.Second.Run();
+        }
+
+        throw new ArgumentException($"Error on processing {Location.GetLog()}");
+    }
+}
+
 
 public sealed class ConditionExpression : ValueExpression<Expression>
 {
     public Expression Condition { get; set; }
     public Expression Then { get; set; }
     public Expression Otherwise { get; set; }
+
+    public override object Run()
+    {
+        if (Condition is ValueExpression<bool> conditionResult)
+        {
+            if (conditionResult.Value)
+            {
+                return Then.Run();
+            }
+            return Otherwise?.Run();
+        }
+
+        throw new ArgumentException($"Error on processing {Condition.Location.GetLog()}");
+    }
 }
 
 public sealed class BinaryExpression : Expression
 {
     [JsonPropertyName("lhs")]
-    public VarExpression LeftHandSide { get; set; }
+    public Expression LeftHandSide { get; set; }
+
     [JsonPropertyName("rhs")]
-    public VarExpression RightHandSide { get; set; }
+    public Expression RightHandSide { get; set; }
 
     [JsonPropertyName("op")]
-    public BinaryOperator Operation { get; set; }
+    public string Operation { get; set; }
+
+    private BinaryOperator GetOperation() => (BinaryOperator)Enum.Parse(typeof(BinaryOperator), Operation, true);
 
     public override object Run()
     {
-        throw new NotImplementedException();
+        var leftSide = RunUntil.Find<ValueExpression>(LeftHandSide);
+        var rightSide = RunUntil.Find<ValueExpression>(RightHandSide);
+
+        switch (GetOperation())
+        {
+            case BinaryOperator.Add:
+                return AddOperation(leftSide, rightSide);
+            case BinaryOperator.Sub:
+                return SubOperation(leftSide, rightSide);
+            case BinaryOperator.Mul:
+                return MultiOperation(leftSide, rightSide);
+            case BinaryOperator.Div:
+                return DivOperation(leftSide, rightSide);
+            case BinaryOperator.Rem:
+                return RemOperation(leftSide, rightSide);
+            case BinaryOperator.Eq:
+                return EqualOperation(leftSide, rightSide);
+            case BinaryOperator.Neq:
+                return NotEqualOperation(leftSide, rightSide);
+            case BinaryOperator.Lt:
+                return LessThanOperation(leftSide, rightSide);
+            case BinaryOperator.Gt:
+                return GreaterThanOperation(leftSide, rightSide);
+            case BinaryOperator.Lte:
+                return LessOrEqualThanOperation(leftSide, rightSide);
+            case BinaryOperator.Gte:
+                return GreaterOrEqualThanOperation(leftSide, rightSide);
+            case BinaryOperator.And:
+                return AndOperation(leftSide, rightSide);
+            case BinaryOperator.Or:
+                return OrOperation(leftSide, rightSide);
+            default:
+                throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+        }
+
+    }
+
+    private Expression AddOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<int> { Value = leftSideIntValue.Value + rightSideIntValue.Value, Location = leftSide.Location };
+            }
+            else if (rightSide is ValueExpression<string> rightSideStrValue)
+            {
+                return new ValueExpression<string> { Value = leftSideIntValue.Value + rightSideStrValue.Value, Location = leftSide.Location };
+            }
+        }
+        else if (leftSide is ValueExpression<string> leftSideStrValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<string> { Value = leftSideStrValue.Value + rightSideIntValue.Value, Location = leftSide.Location };
+            }
+            else if (rightSide is ValueExpression<string> rightSideStrValue)
+            {
+                return new ValueExpression<string> { Value = leftSideStrValue.Value + rightSideStrValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<int> SubOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<int> { Value = leftSideIntValue.Value - rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<int> MultiOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<int> { Value = leftSideIntValue.Value * rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<int> DivOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<int> { Value = leftSideIntValue.Value / rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<int> RemOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<int> { Value = leftSideIntValue.Value % rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> EqualOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value == rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        else if (leftSide is ValueExpression<string> leftSideStrValue)
+        {
+            if (rightSide is ValueExpression<string> rightSideStrValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideStrValue.Value == rightSideStrValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> NotEqualOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value != rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        else if (leftSide is ValueExpression<string> leftSideStrValue)
+        {
+            if (rightSide is ValueExpression<string> rightSideStrValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideStrValue.Value != rightSideStrValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> LessThanOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value < rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> LessOrEqualThanOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value <= rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> GreaterThanOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value < rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> GreaterOrEqualThanOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<int> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<int> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value <= rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> AndOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<bool> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<bool> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value && rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
+    }
+
+    private ValueExpression<bool> OrOperation(ValueExpression leftSide, ValueExpression rightSide)
+    {
+        if (leftSide is ValueExpression<bool> leftSideIntValue)
+        {
+            if (rightSide is ValueExpression<bool> rightSideIntValue)
+            {
+                return new ValueExpression<bool> { Value = leftSideIntValue.Value && rightSideIntValue.Value, Location = leftSide.Location };
+            }
+        }
+        throw new ArgumentException($"Error on processing near {leftSide.Location.GetLog()}");
     }
 }
 
@@ -134,8 +433,22 @@ public sealed class CallExpression : Expression
 {
     public Expression Callee { get; set; }
 
+    public Expression[] Arguments { get; set; }
+
     public override object Run()
     {
-        throw new NotImplementedException();
+        return Callee.Run();
+    }
+}
+
+
+public class RunUntil
+{
+    public static TExpression Find<TExpression>(Expression expression)
+    {
+        if (expression is TExpression tExpression) return tExpression;
+        var expResult = expression.Run();
+        if (expResult is Expression exp) return Find<TExpression>(exp);
+        throw new ArgumentException($"Error on processing {expression.Location.GetLog()}");
     }
 }
